0b38684484a68add17d4b7035202bee9421a8d15
Drew Dimmery <drew.dimmery@gmail.com>
2024-08-13 12:38:38 +0200

    Documentation for MinMaxScaler

--- src/onorm/minmax.py
+++ src/onorm/minmax.py
@@ -4,21 +4,37 @@ from .normalization_base import Normalizer
 
 
 class MinMaxScaler(Normalizer):
-    def __init__(self, n_dim):
+    """**Min-Max Scaling for each covariate**
+
+    The class performs element-wise online updates of a minimum and maximum and
+    then normalizes so that each element, $i$, of the vector $x_t$ is guaranteed to lie
+    within zero and one.
+
+    Define the minimum and maximum at time $t$:
+
+    $$\\textrm{mn}_{ti} = \\min \\{x_{1i},\\dots x_{ti}\\}$$
+
+    $$\\textrm{mx}_{ti} = \\max \\{x_{1i},\\dots x_{ti}\\}$$
+
+    Then normalization is:
+    $$\\frac{x_{ti} - \\textrm{mn}_{ti}}{\\textrm{mx}_{ti} - \\textrm{mn}_{ti}}$$
+    """
+
+    def __init__(self, n_dim: int) -> None:
         self.min = np.array([np.inf] * n_dim)
         self.max = np.array([-np.inf] * n_dim)
 
-    def update_min(self, x):
+    def update_min(self, x: np.ndarray) -> None:
         self.min = np.fmin(self.min, x)
 
-    def update_max(self, x):
+    def update_max(self, x: np.ndarray) -> None:
         self.max = np.fmax(self.max, x)
 
-    def partial_fit(self, x):
+    def partial_fit(self, x: np.ndarray) -> None:
         self.update_min(x)
         self.update_max(x)
 
-    def transform(self, x):
+    def transform(self, x: np.ndarray) -> np.ndarray:
         denom = self.max - self.min
         if np.linalg.norm(denom) <= np.finfo(np.float64).eps:
             denom = 1
--- src/onorm/normalization_base.py
+++ src/onorm/normalization_base.py
@@ -1,5 +1,7 @@
 from abc import ABCMeta, abstractmethod
 
+import numpy as np
+
 
 class Normalizer(metaclass=ABCMeta):
     """**Base class**
@@ -7,17 +9,17 @@ class Normalizer(metaclass=ABCMeta):
     This is the base class for all normalizers. They use a standard API.
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs) -> None:
         pass
 
     @abstractmethod
-    def partial_fit(self, x):
+    def partial_fit(self, x: np.ndarray) -> None:
         raise NotImplementedError
 
     @abstractmethod
-    def transform(self, x):
+    def transform(self, x: np.ndarray) -> np.ndarray:
         raise NotImplementedError
 
-    def partial_fit_transform(self, x):
+    def partial_fit_transform(self, x: np.ndarray) -> np.ndarray:
         self.partial_fit(x)
         return self.transform(x)
