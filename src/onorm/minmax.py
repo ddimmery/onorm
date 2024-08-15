da2910fada6b2512b22cb96e5a2a4f4c362fa1e9
Drew Dimmery <drew.dimmery@gmail.com>
2024-08-15 18:00:44 +0200

    Documentation

--- src/onorm/minmax.py
+++ src/onorm/minmax.py
@@ -4,7 +4,7 @@ from .normalization_base import Normalizer
 
 
 class MinMaxScaler(Normalizer):
-    """**Min-Max Scaling for each covariate**
+    """Min-Max Scaling for each feature
 
     The class performs element-wise online updates of a minimum and maximum and
     then normalizes so that each element, $i$, of the vector $x_t$ is guaranteed to lie
@@ -25,17 +25,33 @@ class MinMaxScaler(Normalizer):
         self.min = np.array([np.inf] * n_dim)
         self.max = np.array([-np.inf] * n_dim)
 
-    def update_min(self, x: np.ndarray) -> None:
+    def _update_min(self, x: np.ndarray) -> None:
         self.min = np.fmin(self.min, x)
 
-    def update_max(self, x: np.ndarray) -> None:
+    def _update_max(self, x: np.ndarray) -> None:
         self.max = np.fmax(self.max, x)
 
     def partial_fit(self, x: np.ndarray) -> None:
-        self.update_min(x)
-        self.update_max(x)
+        """Update the minimum and maximum for each feature.
+
+        Compares the new feature vector to the previous minimum
+        and maximum for each dimension and updates the state.
+
+        Args:
+            x: A 1d array representing a new observation.
+        """
+        self._update_min(x)
+        self._update_max(x)
 
     def transform(self, x: np.ndarray) -> np.ndarray:
+        """Transform the feature vector according to the current state
+
+        If the current minimum and maximum are equal, then the transformation
+        returns $x_{ti} - \\textrm{mn}_{ti}$.
+
+        Args:
+            x: A 1d array representing an observation to normalize.
+        """
         denom = self.max - self.min
         if np.linalg.norm(denom) <= np.finfo(np.float64).eps:
             denom = 1
--- src/onorm/normalization_base.py
+++ src/onorm/normalization_base.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 class Normalizer(metaclass=ABCMeta):
-    """**Base class**
+    """Base class
 
     This is the base class for all normalizers. They use a standard API.
     """
@@ -14,12 +14,37 @@ class Normalizer(metaclass=ABCMeta):
 
     @abstractmethod
     def partial_fit(self, x: np.ndarray) -> None:
+        """Progressive fitting of normalization model
+
+        This method takes a vector of data and updates the normalization
+        model. The specifics of that model depend on the particular implementation.
+
+        Args:
+            x: A 1d array representing a new observation.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def transform(self, x: np.ndarray) -> np.ndarray:
+        """Normalize data
+
+        This method takes a vector of data and uses the normalization model to
+        appropriately transform the data. The specifics of that model depend on the
+        particular implementation.
+
+        Args:
+            x: A 1d array representing an observation to normalize.
+        """
         raise NotImplementedError
 
     def partial_fit_transform(self, x: np.ndarray) -> np.ndarray:
+        """Fit normalization model and transform data
+
+        This method takes a vector of data, updates the normalization model and transforms the
+        supplied data. The specifics of that model depend on the particular implementation.
+
+        Args:
+            x: A 1d array representing a new observation to normalize.
+        """
         self.partial_fit(x)
         return self.transform(x)
